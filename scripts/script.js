30830cec381320291b5001abc9decb7c27146cff
Alan <alan.edward.martin@gmail.com>
2020-04-11 19:33:57 -0400

    added a namespace object, organized code, edited styles, added a sweet alert, made the card elements append to the DOM instead of hard coded into the html(this was done to make shuffling of cards easier)

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,9 @@
 /* eslint-disable */
-// namespacing object
 
-// create array of cards
-let cards = [
+// nameSpacing Object
+const memoryApp = {};
+
+memoryApp.cards = [
   {
     name: 'one',
     image: './card-assets/cardOne.jpg',
@@ -11,7 +12,6 @@ let cards = [
   {
     name: 'one',
     image: './card-assets/cardOne.jpg',
-    dataSet: '',
   },
 
   {
@@ -84,61 +84,112 @@ let cards = [
     image: './card-assets/cardEight.jpg',
   },
 ];
-// Pseudo Code
 
-$(function() {
-  
-  // on click/key press of start button header scrolls up and hides
-  $('.startButton').on('click', playGame);
-  
-  function playGame() {
-    $('header').slideUp(1000, function() {
-      $('.cardContainer').removeClass('hide');
-      // a prompt is then given with instruction on how to play game
-      Swal.fire({
-        title: 'Instructions',
-        text: 'Help all the cacti find their friends and return to the terraium!',
-        icon: '',
-        confirmButtonText: 'Cool',
-      });
-    });
-  }
+// Global Variables
 
-  //shuffle cards array
-  let shuffleCards = (array) => {
-    let newPos;
-    let temp;
+memoryApp.startButton = $('.startButton'); // header start button
+memoryApp.cardContainer = $('.cardContainer'); // div that holds memory cards
+memoryApp.memoryCard = $('.memoryCard'); //memory cards
+memoryApp.firstCard = null;
+memoryApp.secondCard = null;
+memoryApp.hasFlippedCard= null;
 
-    for (let i = array.length - 1; i > 0; i--) {
-      newPos = Math.floor(Math.random() * (i + 1));
-      temp = array[i];
-      array[i] = array[newPos];
-      array[newPos] = temp;
-    }
-    //return new array to new variable
-    return array;
-  }
-  //store shuffled cards array into a randomized card array
-  const randomizedCards = shuffleCards(cards)
-
-  randomizedCards.forEach((card) => {
-    $(".cardContainer").append(`
-  <div class="memoryCard">
-    <img src="./card-assets/cardBack.jpg" alt="cactus patter" class="backFace">
-    <img src="${card.image}" alt="cactus graphic" class="frontFace" data-set="${card.name}">
-  </div>
-  `);
-  });
-
-});
-
-// cards are then shuffled using a for-loop assigning an index to each card, thus randomly assorting them
-// cards are flipped using toggleClass and given a class of flip to the memory card div
-// using if else statement cards are flipped back over by removing flip  class after second click.
-// cards are matched when the data-card are the same, the event handler is removed from matched cards so they can no longer be flipped.
-// when all cards are matched, prompt will appear and congratulate player, player can now choose to play again.
-// cards are reset, and shuffled again.
 
 // init function
-// document.ready
-// call init function
+
+memoryApp.init = () => {
+  // lets play button
+  $(memoryApp.startButton).on('click', memoryApp.playGame);
+  // deals cards to gameboard / card container
+  memoryApp.dealCards();
+  // flips memory cards on user click
+  $('.memoryCard').on('click', memoryApp.flipCard)
+    .keypress('enter', memoryApp.flipCard)
+  
+};
+
+// 1. start game with click of button, slide header up and show gameBoard, and instructions
+
+memoryApp.playGame = () => {
+
+  $('header').slideUp(1000);
+  $('.gameBoard').toggleClass('hide');
+  setTimeout(() => {
+    Swal.fire({
+      title: 'Instructions',
+      text: 'Help all the cacti find their friends and return to the terraium!',
+      icon: '',
+      confirmButtonText: 'Cool',
+    });
+  }, 1000);
+  // call dealCards / deals cards
+};
+
+// 2. shuffle cards and append them to the dom in the cardsContainer using a for loop, using Fisher–Yates shuffle algorithm.
+
+memoryApp.shuffleCards = (array) => {
+  let newPos;
+  let temp;
+
+  for (let i = array.length - 1; i > 0; i--) {
+    newPos = Math.floor(Math.random() * (i + 1));
+    temp = array[i];
+    array[i] = array[newPos];
+    array[newPos] = temp
+  }
+  return array;
+};
+
+// 3. create a new array of random cards from the for loop, and store them into a new variable of randomizedCards, use that new array to append the array/ object information into the DOM, into a div of card container, when user clicks lets play.
+memoryApp.randomizedCards = memoryApp.shuffleCards(memoryApp.cards);
+
+memoryApp.dealCards = () => {
+
+  memoryApp.randomizedCards.forEach((card) => {
+    $(memoryApp.cardContainer).append(`
+    <div class="memoryCard" data-card="${card.name}" tabindex="0">
+      <img src="./card-assets/cardBack.jpg" alt="cactus pattern" class="backFace">
+      <img src="${card.image}" alt="cactus graphic" class="frontFace" >
+    </div>`
+    )
+  })
+};
+
+// 4. When player clicks on card, the div of memoryCard adds a class of flip. The function will count the number of click, allowing only two cards to be chosen at a time.
+
+memoryApp.flipCard = function() {
+  $(this).toggleClass('flip');
+
+  if(!memoryApp.hasFlippedCard) {
+    //first click
+    memoryApp.hasFlippedCard = true;
+    memoryApp.firstCard = this;
+  } else {
+    // second click
+    memoryApp.hasFlippedCard = false;
+    memoryApp.secondCard = this;
+
+    // check for match
+    if (memoryApp.firstCard.dataset.card === memoryApp.secondCard.dataset.card) {
+      // if cards match, remove event listeners 
+      $(memoryApp.firstCard).off('click keypress', memoryApp.flipCard)
+      $(memoryApp.secondCard).off('click keypress', memoryApp.flipCard)
+    } else {
+      // if not matched, remove class of flip
+      setTimeout(() => {
+        $(memoryApp.firstCard).toggleClass('flip')
+        $(memoryApp.secondCard).toggleClass('flip')
+      },800);
+    }
+  }
+};
+
+memoryApp.isFlipped = function() {
+
+};
+
+// document ready
+$(function() {
+  console.log('here');
+  memoryApp.init();
+});
