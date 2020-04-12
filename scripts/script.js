36534b26b557a79a3f32ebc9eb5e422373d8d09a
Alan <alan.edward.martin@gmail.com>
2020-04-11 21:32:33 -0400

    refactored code, added debugging for multiple clicks, multiclicking the start button(which cancelled cards appending to cardContainer

--- scripts/script.js
+++ scripts/script.js
@@ -7,97 +7,115 @@ memoryApp.cards = [
   {
     name: 'one',
     image: './card-assets/cardOne.jpg',
+    alt: 'Cartoon Aloe Vera',
   },
 
   {
     name: 'one',
     image: './card-assets/cardOne.jpg',
+    alt: 'Cartoon Aloe Vera',
   },
 
   {
     name: 'two',
     image: './card-assets/cardTwo.jpg',
+    alt: 'Cartoon Echeveria elegans - aka Mexican Snowball'
   },
 
   {
     name: 'two',
     image: './card-assets/cardTwo.jpg',
+    alt: 'Cartoon Echeveria elegans - aka Mexican Snowball',
+
   },
 
   {
     name: 'three',
     image: './card-assets/cardThree.jpg',
+    alt: 'Cartoon Parodia cactus',
   },
 
   {
     name: 'three',
     image: './card-assets/cardThree.jpg',
+    alt: 'Cartoon Parodia cactus',
   },
 
   {
     name: 'four',
     image: './card-assets/cardFour.jpg',
+    alt: 'Cartoon Saguaro Cactus',
   },
 
   {
     name: 'four',
     image: './card-assets/cardFour.jpg',
+    alt: 'Cartoon Saguaro Cactus',
   },
 
   {
     name: 'five',
     image: './card-assets/cardFive.jpg',
+    alt: 'Cartoon Snake Plant',
   },
 
   {
     name: 'five',
     image: './card-assets/cardFive.jpg',
+    alt: 'Cartoon Snake Plant',
   },
 
   {
     name: 'six',
     image: './card-assets/cardSix.jpg',
+    alt: 'Cartoon Barbary Fig',
   },
-
+  
   {
     name: 'six',
     image: './card-assets/cardSix.jpg',
+    alt: 'Cartoon Barbary Fig',
   },
 
   {
     name: 'seven',
     image: './card-assets/cardSeven.jpg',
+    alt: 'Cartoon Pilosocereus Cactus',
   },
-
+  
   {
     name: 'seven',
     image: './card-assets/cardSeven.jpg',
+    alt: 'Cartoon Pilosocereus Cactus',
   },
 
   {
     name: 'eight',
-    image: './card-assets/cardEight.jpg',
+    image: './card-assets/cardEight.jpg', 
+    alt: 'Cartoon Prickly Pear Cactus',
   },
-
+  
   {
     name: 'eight',
     image: './card-assets/cardEight.jpg',
+    alt: 'Cartoon Prickly Pear Cactus',
   },
 ];
 
 // Global Variables
 
 memoryApp.startButton = $('.startButton'); // header start button
-memoryApp.cardContainer = $('.cardContainer'); // div that holds memory cards
+memoryApp.cardContainer = $('.cardContainer'); // memory card container
 memoryApp.memoryCard = $('.memoryCard'); //memory cards
 memoryApp.firstCard = null;
 memoryApp.secondCard = null;
-memoryApp.hasFlippedCard= null;
+memoryApp.hasFlippedCard= false;
+memoryApp.lockBoard = false;
 
 
 // init function
 
-memoryApp.init = () => {
+memoryApp.init = function() {
   // lets play button
   $(memoryApp.startButton).on('click', memoryApp.playGame);
   // deals cards to gameboard / card container
@@ -110,8 +128,9 @@ memoryApp.init = () => {
 
 // 1. start game with click of button, slide header up and show gameBoard, and instructions
 
-memoryApp.playGame = () => {
+memoryApp.playGame = function() {
 
+  memoryApp.disableStartButton();
   $('header').slideUp(1000);
   $('.gameBoard').toggleClass('hide');
   setTimeout(() => {
@@ -125,9 +144,15 @@ memoryApp.playGame = () => {
   // call dealCards / deals cards
 };
 
+// prevent double clicking of start button which makes cards not load
+memoryApp.disableStartButton = function() {
+  $(memoryApp.startButton).off('click')
+};
+
+
 // 2. shuffle cards and append them to the dom in the cardsContainer using a for loop, using Fisher–Yates shuffle algorithm.
 
-memoryApp.shuffleCards = (array) => {
+memoryApp.shuffleCards = function(array) {
   let newPos;
   let temp;
 
@@ -148,8 +173,8 @@ memoryApp.dealCards = () => {
   memoryApp.randomizedCards.forEach((card) => {
     $(memoryApp.cardContainer).append(`
     <div class="memoryCard" data-card="${card.name}" tabindex="0">
-      <img src="./card-assets/cardBack.jpg" alt="cactus pattern" class="backFace">
-      <img src="${card.image}" alt="cactus graphic" class="frontFace" >
+      <img src="./card-assets/cardBack.jpg" alt="cactus pattern" class="backFace" aria-label="You may select  this memory card">
+      <img src="${card.image}" alt="${card.alt}" class="frontFace">
     </div>`
     )
   })
@@ -158,6 +183,11 @@ memoryApp.dealCards = () => {
 // 4. When player clicks on card, the div of memoryCard adds a class of flip. The function will count the number of click, allowing only two cards to be chosen at a time.
 
 memoryApp.flipCard = function() {
+  
+  if(memoryApp.lockBoard) return;
+  // prevent double click
+  if (this === memoryApp.firstCard) return;
+
   $(this).toggleClass('flip');
 
   if(!memoryApp.hasFlippedCard) {
@@ -173,23 +203,37 @@ memoryApp.flipCard = function() {
   }
 };
 
+// 5. function checks for match using a if statement, the if statement checks to see if cards have matching data values
 memoryApp.isMatched = function() {
-
     if (memoryApp.firstCard.dataset.card === memoryApp.secondCard.dataset.card) {
       // if cards match, remove event listeners 
-      $(memoryApp.firstCard).off('click keypress', memoryApp.flipCard)
-      $(memoryApp.secondCard).off('click keypress', memoryApp.flipCard)
+      memoryApp.disableCards();
     } else {
       // if not matched, remove class of flip
-      setTimeout(() => {
-        $(memoryApp.firstCard).toggleClass('flip')
-        $(memoryApp.secondCard).toggleClass('flip')
-      },800);
+      memoryApp.unflipCards();
     }
 };
 
+// disables cards from being clicked when they are matched.
+memoryApp.disableCards = function() {
+  $(memoryApp.firstCard).off('click keypress', memoryApp.flipCard)
+  $(memoryApp.secondCard).off('click keypress', memoryApp.flipCard)
+}
+
+// flip cards back over if no match is found
+memoryApp.unflipCards = function() {
+
+  memoryApp.lockBoard = true;
+
+  setTimeout(() => {
+    $(memoryApp.firstCard).toggleClass('flip')
+    $(memoryApp.secondCard).toggleClass('flip')
+
+    memoryApp.lockBoard = false;
+  }, 800);
+}
+
 // document ready
 $(function() {
-  console.log('here');
   memoryApp.init();
 });
