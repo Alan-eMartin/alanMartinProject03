cf72721c09279ac23cde5c2d20685352af69730a
Alan <alan.edward.martin@gmail.com>
2020-04-12 21:37:15 -0400

    fixed some spacing, renamed matchedCards to matchCount

--- scripts/script.js
+++ scripts/script.js
@@ -19,14 +19,13 @@ memoryApp.cards = [
   {
     name: 'two',
     image: './card-assets/cardTwo.jpg',
-    alt: 'Cartoon Echeveria elegans - aka Mexican Snowball'
+    alt: 'Cartoon Echeveria elegans - aka Mexican Snowball',
   },
 
   {
     name: 'two',
     image: './card-assets/cardTwo.jpg',
     alt: 'Cartoon Echeveria elegans - aka Mexican Snowball',
-
   },
 
   {
@@ -70,7 +69,7 @@ memoryApp.cards = [
     image: './card-assets/cardSix.jpg',
     alt: 'Cartoon Barbary Fig',
   },
-  
+
   {
     name: 'six',
     image: './card-assets/cardSix.jpg',
@@ -82,7 +81,7 @@ memoryApp.cards = [
     image: './card-assets/cardSeven.jpg',
     alt: 'Cartoon Pilosocereus Cactus',
   },
-  
+
   {
     name: 'seven',
     image: './card-assets/cardSeven.jpg',
@@ -91,10 +90,10 @@ memoryApp.cards = [
 
   {
     name: 'eight',
-    image: './card-assets/cardEight.jpg', 
+    image: './card-assets/cardEight.jpg',
     alt: 'Cartoon Prickly Pear Cactus',
   },
-  
+
   {
     name: 'eight',
     image: './card-assets/cardEight.jpg',
@@ -106,13 +105,12 @@ memoryApp.cards = [
 
 memoryApp.startButton = $('.startButton'); // header start button
 memoryApp.cardContainer = $('.cardContainer'); // memory card container
-memoryApp.memoryCard = $('.memoryCard'); //memory cards
-memoryApp.firstCard = null; // first selected card variable 
+memoryApp.memoryCard = $('.memoryCard'); // memory cards
+memoryApp.firstCard = null; // first selected card variable
 memoryApp.secondCard = null; // second selected card variable
-memoryApp.hasFlippedCard= false; // has card flipped boolean
+memoryApp.hasFlippedCard = false; // has card flipped boolean
 memoryApp.lockBoard = false; // lock board for preventing multiclicks boolean
-memoryApp.matchedCard = 0; // variable for counting matches
-
+memoryApp.matchCount = 0; // variable for counting matches
 
 // init function
 
@@ -122,22 +120,22 @@ memoryApp.init = function() {
   // deals cards to gameboard / card container
   memoryApp.dealCards();
   // flips memory cards on user click
-  $('.memoryCard').on('click', memoryApp.flipCard)
-    .keypress('enter', memoryApp.flipCard)
-  
+  $('.memoryCard')
+    .on('click', memoryApp.flipCard)
+    .keypress('enter', memoryApp.flipCard);
 };
 
 // 1. start game with click of button, slide header up and show gameBoard, and instructions
 
 memoryApp.playGame = function() {
-
   memoryApp.disableStartButton();
   $('header').slideUp(1000);
   $('.gameBoard').toggleClass('hide');
   setTimeout(() => {
     Swal.fire({
       title: 'Instructions',
-      text: 'Help all the cacti find their friends and return to the terrarium!',
+      text:
+        'Help all the cacti find their friends and return to the terrarium!',
       icon: '',
       confirmButtonText: 'Play Game ',
     });
@@ -146,9 +144,8 @@ memoryApp.playGame = function() {
 
 // prevents double clicking of start button which makes cards prevents card from loading
 memoryApp.disableStartButton = function() {
-  $(memoryApp.startButton).off('click')
+  $(memoryApp.startButton).off('click');
 };
-
 
 // 2. shuffles cards and appends them to the dom in the cardsContainer div using a "for" loop (Fisher–Yates shuffle algorithm)
 
@@ -160,7 +157,7 @@ memoryApp.shuffleCards = function(array) {
     newPos = Math.floor(Math.random() * (i + 1));
     temp = array[i];
     array[i] = array[newPos];
-    array[newPos] = temp
+    array[newPos] = temp;
   }
   return array;
 };
@@ -169,76 +166,70 @@ memoryApp.shuffleCards = function(array) {
 memoryApp.randomizedCards = memoryApp.shuffleCards(memoryApp.cards);
 
 memoryApp.dealCards = () => {
-
-  memoryApp.randomizedCards.forEach((card) => {
+  memoryApp.randomizedCards.forEach(card => {
     $(memoryApp.cardContainer).append(`
     <div class="memoryCard" data-card="${card.name}" tabindex="0">
       <img src="./card-assets/cardBack.jpg" alt="cactus pattern" class="backFace" aria-label="You may select  this memory card">
       <img src="${card.image}" alt="${card.alt}" class="frontFace">
-    </div>`
-    )
-  })
+    </div>`);
+  });
 };
 
 // 4. When player clicks on card, the div of memoryCard adds a class of flip. The function will count the number of click, allowing only two cards to be chosen at a time.
 
 memoryApp.flipCard = function() {
-  
-  if(memoryApp.lockBoard) return;
+  if (memoryApp.lockBoard) return;
   // prevent double click
   if (this === memoryApp.firstCard) return;
 
   $(this).toggleClass('flip');
 
-  if(!memoryApp.hasFlippedCard) {
-    //first click
+  if (!memoryApp.hasFlippedCard) {
+    // first click
     memoryApp.hasFlippedCard = true;
     memoryApp.firstCard = this;
   } else {
     // second click
     memoryApp.hasFlippedCard = false;
     memoryApp.secondCard = this;
-    //check for match
+    // check for match
     memoryApp.isMatched();
   }
 };
 
 // 5. function checks for match using an if statement, the if statement checks to see if cards have matching data values
 memoryApp.isMatched = function() {
-
-    if (memoryApp.firstCard.dataset.card === memoryApp.secondCard.dataset.card) {
-      // if cards match, remove event listeners 
-      memoryApp.disableCards();
-      memoryApp.matchedCard ++;
-      // if all eight matches found show you one alert
-      if(memoryApp.matchedCard === 8) {
-        memoryApp.gameOver()
-      }
-    } else {
-      // if not matched, remove class of flip
-      memoryApp.unflipCards();
+  if (memoryApp.firstCard.dataset.card === memoryApp.secondCard.dataset.card) {
+    // if cards match, remove event listeners
+    memoryApp.disableCards();
+    memoryApp.matchCount++;
+    // if all eight matches found show you one alert
+    if (memoryApp.matchedCard == 8) {
+      memoryApp.gameOver();
     }
+  } else {
+    // if not matched, remove class of flip
+    memoryApp.unflipCards();
+  }
 };
 
 // 6. disables cards from being clicked when they are matched.
 memoryApp.disableCards = function() {
-  $(memoryApp.firstCard).off('click keypress', memoryApp.flipCard)
-  $(memoryApp.secondCard).off('click keypress', memoryApp.flipCard)
-}
+  $(memoryApp.firstCard).off('click keypress', memoryApp.flipCard);
+  $(memoryApp.secondCard).off('click keypress', memoryApp.flipCard);
+};
 
 // 7. flip cards back over if no match is found
 memoryApp.unflipCards = function() {
-
   memoryApp.lockBoard = true;
-
+  // time out allows cards finishing flipping over, if no time out set second cards flip class is remove before it even flips
   setTimeout(() => {
-    $(memoryApp.firstCard).toggleClass('flip')
-    $(memoryApp.secondCard).toggleClass('flip')
+    $(memoryApp.firstCard).toggleClass('flip');
+    $(memoryApp.secondCard).toggleClass('flip');
 
     memoryApp.lockBoard = false;
   }, 800);
-
-}
+};
 
 // 8. when all cards are matched, message comes up and asks player if they would like to try again. On click the page is reloaded.
 memoryApp.gameOver = function() {
@@ -249,12 +240,11 @@ memoryApp.gameOver = function() {
     confirmButtonText: 'Try Again?',
   });
   // click play again and page reloads
-  setTimeout(() => {
-    $(document).click(function () {
-      location.reload();
-    }), 1500;
-  })
-}
+  $(document).then(function() {
+    location.reload();
+  });
+  
+};
 
 // document ready
 $(function() {
